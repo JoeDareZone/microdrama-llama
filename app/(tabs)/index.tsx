83dcc70a0d16e3cf37e96623dcc1e5120bf02ba5
Joe Ulyatt <joewhocodes@gmail.com>
2025-04-12 19:55:19 +0200

    feat: :sparkles: Added google auth

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,39 @@
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import {
+	GoogleSignin,
+	isSuccessResponse,
+} from '@react-native-google-signin/google-signin'
+import { useEffect, useState } from 'react'
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity } from 'react-native'
 
 export default function HomeScreen() {
+	const [error, setError] = useState({})
+
+	let statusCodes
+
+	useEffect(() => {
+		GoogleSignin.configure()
+	}, [])
+
+	const signIn = async () => {
+		try {
+			await GoogleSignin.hasPlayServices()
+			const response = await GoogleSignin.signIn()
+			if (isSuccessResponse(response)) {
+				setError({ userInfo: response.data })
+			} else {
+				// sign in was cancelled by user
+			}
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
 	return (
 		<SafeAreaView>
 			<Text className='text-right bg-red-600'>HEy</Text>
+      <TouchableOpacity onPress={signIn}>
+        <Text>Sign In!</Text>
+      </TouchableOpacity>
 		</SafeAreaView>
 	)
 }
